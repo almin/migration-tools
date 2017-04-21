29e7b1c76d41d785f0ad257b264b5a62234cb186
azu <azuciao@gmail.com>
2017-04-21 23:47:00 +0900

    Update main

---
+++ index.js
@@ -0,0 +1,6 @@
+// MIT © 2017 azu
+"use strict";
+module.exports = {
+    storeGetStateReturnObjectToFlat: require("./scripts/store-get-state-return-object-to-flat"),
+    storeGroupArguments: require("./scripts/store-group-arguments")
+};
